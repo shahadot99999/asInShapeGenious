beca2a8b5542761eaefb314220145433ad4d47f6
shahadot99999 <shahadot99999@gmail.com>
2024-11-23 19:05:21 +0600

    height and weight are added

--- scripts/triangle-calculate.js
+++ scripts/triangle-calculate.js
@@ -1,3 +1,15 @@
 function calculateTriangleArea(){
-    console.log('inside Function');
+    
+    // get triangle base value
+    // console.log('inside Function');
+    const triangleBaseInput = document.getElementById('traiangle-base');
+    const traiangleBaseInputText = triangleBaseInput.value;
+    const base = parseFloat(traiangleBaseInputText)
+    console.log( base);
+
+    // get triangle height value
+    const triangleHeightInput = document.getElementById('traiangle-height');
+    const triangleHeightText = triangleHeightInput.value;
+    const height = parseFloat(triangleHeightText)
+    console.log(height);;
 }
